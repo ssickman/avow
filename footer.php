c91eb6c02b9f1663c7f355aa750aa3ed24ddd203
ssickman <sickman@gmail.com>
2014-03-28 14:45:36 -0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+				<div class="inner">
+					<p class="copyright">
+						&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> <img class="for-all" src="wp-content/themes/avow/img/for-all.png">
+					</p>
+				</div>
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Homepage Template
+*/
+?>
+<?php get_header(); ?>
+
+	<main role="main">
+		<section id="banner">
+			
+			
+		
+			<img id="home-banner" src="wp-content/themes/avow/img/homepage.jpg">
+		</section>
+		
+		<section id="content" class="top-box-shadow">
+			<p>
+				kas;fl asdlj as;ldfjk ;alsdfjal ;sfjasl;dfjasl;fj aslfsdjkgwp9rywioury ewiuryw iueryw oirrwrywoir yowryro
+			</p>
+			<p>
+				kas;fl asdlj as;ldfjk ;alsdfjal ;sfjasl;dfjasl;fj aslfsdjkgwp9rywioury ewiuryw iueryw oirrwrywoir yowryro
+			</p>
+			<p>
+				kas;fl asdlj as;ldfjk ;alsdfjal ;sfjasl;dfjasl;fj aslfsdjkgwp9rywioury ewiuryw iueryw oirrwrywoir yowryro
+			</p>
+			<p>
+				kas;fl asdlj as;ldfjk ;alsdfjal ;sfjasl;dfjasl;fj aslfsdjkgwp9rywioury ewiuryw iueryw oirrwrywoir yowryro
+			</p>
+		</section>
+	</main>
+
+<?php //get_sidebar(); ?>
+
+<?php get_footer(); ?>
