e27cfce4aca823fb7895cba98e837d4df84ebe46
ssickman <sickman@gmail.com>
2014-04-09 18:50:30 -0700

    css changes

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		<link href='http://fonts.googleapis.com/css?family=Oxygen:400,700,300|Trocchi|Revalia' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Oxygen:400,700,300|Trocchi' rel='stylesheet' type='text/css'>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
