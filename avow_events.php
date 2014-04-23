6b3831870f8689854c89e17266846888739229d3
ssickman <sickman@gmail.com>
2014-04-23 01:16:05 -0700

    testing

--- avow_events.php
+++ avow_events.php
@@ -4,6 +4,6 @@ add_action( 'admin_head', 'add_menu_icons_styles' );
 add_action('admin_menu', function(){
 
 	add_menu_page('Booked Events', 'Events', 'read', 'avow_events', function(){
-		echo 'whatup!?';
+		echo 'whatup !?';
 	}, 'dashicons-calendar', '26.0001');
 });
